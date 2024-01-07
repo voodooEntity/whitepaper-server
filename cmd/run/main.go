93bd69f657dfc83a96e1b424e27f672210f23954
laughingman <webmaster@laughingman.dev>
2024-01-07 20:10:46 +0100

    update run to start server

--- cmd/run/main.go
+++ cmd/run/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/voodooEntity/archivist"
 	"github.com/voodooEntity/gits"
-	"github.com/voodooEntity/gits/src/query"
-	"github.com/voodooEntity/gits/src/transport"
 	"github.com/voodooEntity/gits/src/types"
 	"github.com/voodooEntity/whitepaper-server/src/config"
+	"github.com/voodooEntity/whitepaper-server/src/server"
 )
 
 func main() {
@@ -26,16 +24,5 @@ func main() {
 		PersistenceChannelBufferSize: 10000000,
 	})
 
-	gits.MapTransportData(
-		transport.TransportEntity{
-			ID:         -1,
-			Context:    "ABC",
-			Type:       "Something",
-			Value:      "asdasd",
-			Properties: map[string]string{"foo": "bar"},
-		})
-
-	qry := query.New().Read("Something")
-	ret := query.Execute(qry)
-	fmt.Print("%+v", ret)
+	server.Start()
 }
