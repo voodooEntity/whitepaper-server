dd5c52beef2249a662ac2e883051ab880fbe84e2
laughingman <webmaster@laughingman.dev>
2024-01-07 21:17:51 +0100

    fix create and update logic

--- src/whitepaper/whitepaper.go
+++ src/whitepaper/whitepaper.go
@@ -16,14 +16,22 @@ type WhitePaper struct {
 }
 
 func (self *WhitePaper) StoreOrUpdate() *WhitePaper {
-	gits.MapTransportData(
-		transport.TransportEntity{
-			ID:         0,
-			Type:       entityName,
-			Context:    "",
-			Value:      self.InstanceId,
-			Properties: map[string]string{"Hash": self.Hash, "ClientID": self.ClientID, "Content": self.Content},
-		})
+	qry := query.New().Read("Whitepaper").Match("Value", "==", self.InstanceId)
+	ret := query.Execute(qry)
+	if 0 == ret.Amount {
+		gits.MapTransportData(
+			transport.TransportEntity{
+				ID:         0,
+				Type:       entityName,
+				Context:    "",
+				Value:      self.InstanceId,
+				Properties: map[string]string{"Hash": self.Hash, "ClientID": self.ClientID, "Content": self.Content},
+			})
+	} else {
+		qry = query.New().Update("Whitepaper").Match("Value", "==", self.InstanceId).Set("Properties.Content", self.Content).Set("Properties.Hash", self.Hash).Set("Properties.ClientID", self.ClientID)
+		query.Execute(qry)
+	}
+
 	return self
 }
 
